e071d2f397675631497a252cf9dd5c335481bbbb
Your Name <n_akio165@taupe.plala.or.jp>
2018-09-08 15:18:50 +0900

    create class component and function component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = "Hi, Tom!";
-    const dom = <h1>{greeting}</h1>;
-    //return dom;
-    return <input type="text" onClick={() => {console.log("I am clicked!")}} />;
+//class App extends Component {
+//  render() {
+//    const greeting = "Hi, Tom!";
+//    const dom = <h1>{greeting}</h1>;
+//    //return dom;
+//    return <input type="text" onClick={() => {console.log("I am clicked!")}} />;
+//
+//  }
+//}
 
-  }
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
